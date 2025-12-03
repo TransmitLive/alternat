fac9e15b4d5a22d5231c564a9c73131e91c13c3a
Pavel Vaysberg <pavel@transmit.live>
2025-12-03 11:35:07 -0500

    Merge upstream changes from forked chime repository (#6)

--- main.tf
+++ main.tf
@@ -11,6 +11,8 @@ locals {
     }
   ]
 
+  nat_instance_image_id = var.nat_ami == "" ? "resolve:ssm:/aws/service/ami-amazon-linux-latest/al2023-ami-minimal-kernel-default-${var.architecture}" : var.nat_ami
+
   nat_instance_ingress_sgs = concat(var.ingress_security_group_ids, [aws_security_group.nat_lambda.id])
 
   all_route_tables = flatten([
@@ -31,28 +33,33 @@ locals {
     }
     : {}
   )
-  lambda_endpoint = (
-    var.enable_lambda_endpoint
-    ? {
-      lambda = {
-        service             = "lambda"
-        private_dns_enabled = true
-        subnet_ids          = local.az_private_subnets
-        tags                = { Name = "lambda-vpc-endpoint" }
-      }
-    }
-    : {}
-  )
-  endpoints = merge(local.ec2_endpoint, local.lambda_endpoint)
 
   # Must provide exactly 1 EIP per AZ
   # var.nat_instance_eip_ids ignored if doesn't match AZ count
   reuse_nat_instance_eips = length(var.nat_instance_eip_ids) == length(var.vpc_az_maps)
-  nat_instance_eip_ids    = local.reuse_nat_instance_eips ? var.nat_instance_eip_ids : aws_eip.nat_instance_eips[*].id
+  nat_instance_eip_ids    = local.reuse_nat_instance_eips ? var.nat_instance_eip_ids : (var.prevent_destroy_eips ? aws_eip.protected_nat_instance_eips[*].id : aws_eip.nat_instance_eips[*].id)
+  nat_instance_eips       = var.prevent_destroy_eips ? aws_eip.protected_nat_instance_eips : aws_eip.nat_instance_eips
+  nat_gateway_eips        = var.prevent_destroy_eips ? aws_eip.protected_nat_gateway_eips : aws_eip.nat_gateway_eips
+}
+
+resource "aws_eip" "protected_nat_instance_eips" {
+  count = (local.reuse_nat_instance_eips
+    ? 0
+  : var.prevent_destroy_eips ? length(var.vpc_az_maps) : 0)
+
+  tags = merge(var.tags, {
+    "Name" = "alternat-instance-${count.index}"
+  })
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "aws_eip" "nat_instance_eips" {
-  count = local.reuse_nat_instance_eips ? 0 : length(var.vpc_az_maps)
+  count = (local.reuse_nat_instance_eips
+    ? 0
+  : (var.prevent_destroy_eips ? 0 : length(var.vpc_az_maps)))
 
   tags = merge(var.tags, {
     "Name" = "alternat-instance-${count.index}"
@@ -284,42 +291,46 @@ resource "aws_iam_role_policy" "alternat_lifecycle_hook" {
   role   = aws_iam_role.alternat_lifecycle_hook.name
 }
 
-
-data "aws_ami" "amazon_linux_2" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "owner-alias"
-    values = ["amazon"]
-  }
-
-  filter {
-    name   = "architecture"
-    values = [var.architecture]
-  }
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm*"]
-  }
-}
-
 data "cloudinit_config" "config" {
   for_each = { for obj in var.vpc_az_maps : obj.az => obj.route_table_ids }
 
   gzip          = true
   base64_encode = true
+
+  dynamic "part" {
+    for_each = var.nat_instance_user_data_pre_install != "" ? [1] : []
+
+    content {
+      content_type = "text/x-shellscript"
+      content      = var.nat_instance_user_data_pre_install
+    }
+  }
+
   part {
     content_type = "text/x-shellscript"
     content = templatefile("${path.module}/alternat.conf.tftpl", {
-      eip_allocation_ids_csv = join(",", local.nat_instance_eip_ids),
-      route_table_ids_csv    = join(",", each.value)
+      eip_allocation_ids_csv  = join(",", local.nat_instance_eip_ids),
+      route_table_ids_csv     = join(",", each.value),
+      enable_ssm              = var.enable_ssm,
+      enable_cloudwatch_agent = var.enable_cloudwatch_agent
     })
   }
+
   part {
     content_type = "text/x-shellscript"
     content      = file("${path.module}/scripts/alternat.sh")
+  }
+
+  dynamic "part" {
+    for_each = var.enable_cloudwatch_agent ? [1] : []
+
+    content {
+      content_type = "text/x-shellscript"
+      content = templatefile("${path.module}/cwagent.json.tftpl", {
+        cloudwatch_namespace  = var.cloudwatch_namespace,
+        cloudwatch_interfaces = jsonencode(var.cloudwatch_interfaces)
+      })
+    }
   }
 
   dynamic "part" {
@@ -334,6 +345,27 @@ data "cloudinit_config" "config" {
 
 resource "aws_launch_template" "nat_instance_template" {
   for_each = { for obj in var.vpc_az_maps : obj.az => obj.route_table_ids }
+
+  name_prefix = var.nat_instance_name_prefix
+
+  image_id = local.nat_instance_image_id
+
+  # Conditional block device mapping for AL2023 Minimal AMI.
+  # By default the root volume is only 2GB and not enough free space
+  # to safely install and use the CloudWatch Agent.
+  dynamic "block_device_mappings" {
+    for_each = (try(strcontains(local.nat_instance_image_id, "al2023-ami-minimal"), false) && var.enable_cloudwatch_agent) ? [1] : []
+
+    content {
+      device_name = "/dev/xvda"
+
+      ebs {
+        volume_size = 3
+        volume_type = "gp3"
+        encrypted   = true
+      }
+    }
+  }
 
   dynamic "block_device_mappings" {
     for_each = try(var.nat_instance_block_devices, {})
@@ -357,9 +389,9 @@ resource "aws_launch_template" "nat_instance_template" {
     name = aws_iam_instance_profile.nat_instance.name
   }
 
-  image_id = var.nat_ami == "" ? data.aws_ami.amazon_linux_2.id : var.nat_ami
-
   instance_type = var.nat_instance_type
+
+  key_name = var.nat_instance_key_name
 
   metadata_options {
     http_endpoint               = "enabled"
@@ -378,7 +410,6 @@ resource "aws_launch_template" "nat_instance_template" {
   }
 
   tags = var.tags
-
   tag_specifications {
     resource_type = "instance"
 
@@ -387,6 +418,13 @@ resource "aws_launch_template" "nat_instance_template" {
     })
   }
 
+  tag_specifications {
+    resource_type = "volume"
+
+    tags = merge(var.tags, {
+      alterNATInstance = "true",
+    })
+  }
   user_data = data.cloudinit_config.config[each.key].rendered
 }
 
@@ -425,6 +463,17 @@ resource "aws_security_group_rule" "nat_instance_ip_range_ingress" {
   to_port                  = 0
   security_group_id        = aws_security_group.nat_instance.id
   cidr_blocks = local.all_vpc_cidr_ranges
+}
+
+resource "aws_security_group_rule" "nat_instance_ipv6_range_ingress" {
+  count = length(var.ingress_security_group_ipv6_cidr_blocks) > 0 ? 1 : 0
+
+  type              = "ingress"
+  protocol          = "-1"
+  from_port         = 0
+  to_port           = 0
+  security_group_id = aws_security_group.nat_instance.id
+  ipv6_cidr_blocks  = var.ingress_security_group_ipv6_cidr_blocks
 }
 
 ### NAT instance IAM
@@ -466,12 +515,19 @@ resource "aws_iam_role_policy_attachment" "ssm" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
+resource "aws_iam_role_policy_attachment" "cloudwatch" {
+  count      = var.enable_cloudwatch_agent ? 1 : 0
+  role       = aws_iam_role.alternat_instance.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
+
 data "aws_iam_policy_document" "alternat_ec2_policy" {
   statement {
     sid    = "alterNATInstancePermissions"
     effect = "Allow"
     actions = [
       "ec2:ModifyInstanceAttribute",
+      "ec2:DescribeInstanceAttribute"
     ]
     resources = ["*"]
     condition {
@@ -501,7 +557,7 @@ data "aws_iam_policy_document" "alternat_ec2_policy" {
     ]
     resources = [
       for route_table in local.all_route_tables
-      : "arn:aws:ec2:${data.aws_region.current.name}:${data.aws_caller_identity.current.id}:route-table/${route_table}"
+      : "arn:aws:ec2:${data.aws_region.current.id}:${data.aws_caller_identity.current.id}:route-table/${route_table}"
     ]
   }
 
@@ -533,11 +589,25 @@ resource "aws_iam_role_policy" "alternat_additional_policies" {
 }
 
 ## NAT Gateway used as a backup route
+resource "aws_eip" "protected_nat_gateway_eips" {
+  for_each = {
+    for obj in var.vpc_az_maps
+    : obj.az => obj.public_subnet_id
+    if var.create_nat_gateways && var.prevent_destroy_eips
+  }
+  tags = merge(var.tags, {
+    "Name" = "alternat-gateway-eip"
+  })
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
 resource "aws_eip" "nat_gateway_eips" {
   for_each = {
     for obj in var.vpc_az_maps
     : obj.az => obj.public_subnet_id
-    if var.create_nat_gateways
+    if var.create_nat_gateways && !var.prevent_destroy_eips
   }
   tags = merge(var.tags, {
     "Name" = "alternat-gateway-eip"
@@ -550,7 +620,7 @@ resource "aws_nat_gateway" "main" {
     : obj.az => obj.public_subnet_id
     if var.create_nat_gateways
   }
-  allocation_id = aws_eip.nat_gateway_eips[each.key].id
+  allocation_id = var.prevent_destroy_eips ? aws_eip.protected_nat_gateway_eips[each.key].id : aws_eip.nat_gateway_eips[each.key].id
   subnet_id     = each.value
   tags = merge(var.tags, {
     Name = "alternat-${each.key}"
@@ -569,7 +639,7 @@ locals {
 }
 
 resource "aws_security_group" "vpc_endpoint" {
-  count = length(local.endpoints) > 0 ? 1 : 0
+  count = length(local.ec2_endpoint) > 0 ? 1 : 0
 
   name_prefix = "ec2-vpc-endpoints-"
   description = "Allow TLS from the VPC CIDR to the AWS API."
@@ -595,13 +665,13 @@ resource "aws_security_group" "vpc_endpoint" {
 }
 
 module "vpc_endpoints" {
-  count = length(local.endpoints) > 0 ? 1 : 0
+  count = length(local.ec2_endpoint) > 0 ? 1 : 0
 
   source             = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
   version            = "~> 3.14.0"
   vpc_id             = var.vpc_id
   security_group_ids = [aws_security_group.vpc_endpoint[0].id]
-  endpoints          = local.endpoints
+  endpoints          = local.ec2_endpoint
   tags               = var.tags
 }
 
